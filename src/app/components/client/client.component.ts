0577fb876d686d4c95aefe7f05718ea1357383a3
Jesus Benitez Cerezo <jesus.benitez-cerezo@iesruizgijon.com>
2024-05-15 14:20:24 +0200

    Modals en client (contenido y manejo)

--- src/app/components/client/client.component.ts
+++ src/app/components/client/client.component.ts
@@ -1,10 +1,46 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-client',
   templateUrl: './client.component.html',
   styleUrl: './client.component.css'
 })
-export class ClientComponent {
+export class ClientComponent implements OnInit{
+  clientError:string='';
+  clientForm=this.formBuilder.group({
+    name: ['', [Validators.required, Validators.maxLength(50)]],
+    surname: ['', [Validators.required, Validators.maxLength(50)]],
+    email: ['', [Validators.required, Validators.email]],
+    password: ['', Validators.required],
+    phone: ['', [Validators.required, Validators.pattern('^[0-9]{9}$')]]
+  })
+
+  constructor(private formBuilder:FormBuilder) {
+    
+  }
+
+  ngOnInit(): void {
+  }
+
+  get name() {
+    return this.clientForm.controls.name;
+  }
+
+  get surname() {
+    return this.clientForm.controls.surname;
+  }
+
+  get phone() {
+    return this.clientForm.controls.phone;
+  }
+
+  get email() {
+    return this.clientForm.controls.email;
+  }
+
+  get password() {
+    return this.clientForm.controls.password;
+  }
 
 }
